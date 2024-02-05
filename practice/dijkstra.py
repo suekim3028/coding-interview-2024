71c13b677191b1a664076e46b68eb71d33f5f31a
Suebin Kim <k.sue3028@gmail.com>
2024-02-05 16:08:52 +0900

    feat: practice dijkstra method2 again

--- practice/dijkstra.py
+++ practice/dijkstra.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+import heapq
 
 
 # 방문한 노드들에 대한 정보 가짐
@@ -47,11 +48,42 @@ def try1():
                 dist[to_n] = dist[smallest] + d
 
     print(dist)
+
+
+input = sys.stdin.readline
+def try2():
+    INF = int(10e9)
+    n,m = map(int, input().split())
+    start = int(input())
+    
+    dist = [INF] * (n+1)
+
+    graph = [[] for _ in range(n+1)] 
+
+    for _ in range(m):
+        from_n, to_n, d = map(int, input().split())
+        graph[from_n].append((to_n, d))
+
+
+    dist[start] = 0
+
+    q = []
+    heapq.heappush(q, (start, 0))
+
+    while(q):
+        # 가운데 애를 꺼내는 중 -> 우선 현재로서는 dist -> to_n 이 젤 짧아야함
+        pass_n, d = heapq.heappop(q)
+        if(dist[pass_n]<d):
+            continue
         
+        for to_n, to_dist in graph[pass_n]:
+            if(dist[to_n]> d+to_dist ):
+                dist[to_n] = d+to_dist
+                heapq.heappush(q,(to_n, d+to_dist))
+    print(dist)
 
 
-
-try1()
+try2()
 
 
 
