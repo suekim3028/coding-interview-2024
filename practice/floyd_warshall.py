662717f5128084b20f03cfb002b515ff14beb78b
Suebin Kim <k.sue3028@gmail.com>
2024-02-05 16:28:26 +0900

    feat: practice fw again

--- practice/floyd_warshall.py
+++ practice/floyd_warshall.py
@@ -1,27 +1,24 @@
-def fn():
+import sys
+
+input = sys.stdin.readline
+
+def try1():
     INF = int(10e9)
-    n = int(input())
-    m = int(input())
-    graph = [[INF]*(n+1) for i in range(n+1)] # *(n+1) 로 하면 depth 하나 이상으로 올라갔을때 pointer 됨 ㄷㄷ
-    
-    for i in range(1,n+1):
-        graph[i][i] = 0
-    
+    n,m = map(int, input().split())
+    dist = [[INF]*(n+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        dist[i][i] = 0
+
     for _ in range(m):
-        from_n, to_n, dist = map(int, input().split())
-        graph[from_n][to_n] = dist
-    
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            for k in range(1,n+1):
-                if(graph[j][k] > graph[j][i] + graph[i][k]):
-                    graph[j][k] = graph[j][i] + graph[i][k]
-    
+        from_n, to_n, d = map(int, input().split())
+        dist[from_n][to_n] = d
+
     for i in range(1,n+1):
         for j in range(1, n+1):
-            item = graph[i][j]
-            print("INF" if item == INF else item, end = " ")
-        print() 
+            for k in range(1, n+1):
+                if(dist[i][k] > dist[i][j] + dist[j][k]):
+                    dist[i][k] = dist[i][j] + dist[j][k]
+    print(dist)     
 
-fn()
-
+try1()
